9283916abe396b69838b5302f625210c30a3865c
Z1Epime <z1epime@gmx.de>
2022-02-02 22:05:50 +0100

    renamed SpecializationNavigationViewContentPage
    new name is SpecializationView

--- WowSpellidManager/WinUI3/Views/ClassesView.xaml.cs
+++ WowSpellidManager/WinUI3/Views/ClassesView.xaml.cs
@@ -66,7 +66,7 @@ namespace WowSpellidManager.WinUI3.Views
 
         private void specializationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
         {
-            specializationFrame.Content = new SpecializationNavigationViewContentPage(specializationView.SelectedItem);
+            specializationFrame.Content = new SpecializationView(specializationView.SelectedItem);
             AddSpellButton.Visibility = Visibility.Visible;
         }
 
--- WowSpellidManager/WinUI3/Views/SpecializationView.xaml.cs
+++ WowSpellidManager/WinUI3/Views/SpecializationView.xaml.cs
@@ -21,9 +21,9 @@ namespace WowSpellidManager.WinUI3.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class SpecializationNavigationViewContentPage : Page
+    public sealed partial class SpecializationView : Page
     {
-        public SpecializationNavigationViewContentPage(object Specialization)
+        public SpecializationView(object Specialization)
         {
             InitializeComponent();
             spellListView.DataContext = Specialization;
