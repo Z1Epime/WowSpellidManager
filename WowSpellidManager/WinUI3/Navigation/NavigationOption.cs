d7c628004c9ac7a9989b0d559579a2d5135d9caa
Z1Epime <z1epime@gmx.de>
2022-02-02 21:03:49 +0100

    General changes
    - added NavigationView for main Navigation
    - added SettingsView
    - added static Navigation lists and Items for Navigation, for main navigation

---
+++ WowSpellidManager/WinUI3/Navigation/NavigationOption.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WowSpellidManager.WinUI3.Navigation
+{
+    public class NavigationOption
+    {
+        #region Properties
+
+        /// <summary>
+        /// The name of the NavigationOption
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// The Glyph (a symbol) of the NavigationOption
+        /// </summary>
+        public Symbol Glyph { get; set; }
+
+        #endregion
+
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public NavigationOption(string aName, Symbol aGlyph)
+        {
+            Name = aName;
+            Glyph = aGlyph;
+        }
+
+        #endregion
+    }
+}
